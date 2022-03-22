5ebb7fa24f97faea9b318294faf3d1f27b746c65
gongjian <225763904@qq.com>
2022-03-22 09:44:28 +0800

    生成jwtToken

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,19 @@
 const router = require('koa-router')()
 const User = require("../models/userSchema")
 const util = require("../utils/utils")
+const jwt = require("jsonwebtoken")
 router.prefix('/users')
 router.post("/login",async (ctx)=>{
   try{
     const {userName,userPwd} = ctx.request.body
     const res = await User.findOne({userName,userPwd})
+    const token = jwt.sign({
+      data:res._doc,
+    },"imooc",{expiresIn:30})
     if(res){
       let initRes = {
         code:200,
-        data:res,
+        data:{...res._doc,token},
         msg:"请求成功"
       }
       ctx.body = util.success(initRes)
